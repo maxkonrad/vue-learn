91897d6113dd36e76daf760887dce0d32ee20ac3
maxkonrad <furkanselek@outlook.com>
2024-01-15 02:57:48 +0300

    methods

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const vm = Vue.createApp({ //short for view-model
     }
 }).mount("#app1")
 
-setTimeout(() => {
-    vm.firstName = "Yasin" //normally you can't access to that this way, you had to do vm.data.firstname etc. this is called !!!PROXY GAIN!!!
-}, 2000)
+// setTimeout(() => {
+//     vm.firstName = "Yasin" //normally you can't access to that this way, you had to do vm.data.firstname etc. this is called !!!PROXY GAIN!!!
+// }, 2000)
 
 
 // Vue.createApp({

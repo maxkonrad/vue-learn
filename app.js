7121bb53c9065a4a397b1dcc6737e8b2d10ab418
maxkonrad <furkanselek@outlook.com>
2024-01-16 22:12:56 +0300

    listening to events

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ const vm = Vue.createApp({
             firstName: "John",
             lastName: "Doe",
             url: "https://google.com",
-            raw_url: "<p><a href='https://google.com' target='_blank'>Google</a></p>"
-            // THIS IS RAW HTML BINDING AN MAY CAUSE SECURITY RISKS LIKE XSS
-            
+            raw_url: "<p><a href='https://google.com' target='_blank'>Google</a></p>",
+            age: 20            
         }
     },
     methods: {
         fullName(){
             return `${this.firstName} ${this.lastName.toUpperCase()}`
+        },
+        increment(){
+            this.age++
+        },
+        updateLastName(event){
+            this.lastName = event.target.value
         }
     }
 }).mount("#app")
